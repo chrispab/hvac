5562a0699a9b230bfa62ccd90bd098cd3ed14cef
chrispab <cbattisson@gmail.com>
2022-09-07 00:02:43 +0100

    pin

--- hardware/raspberrypi/LightHardware.js
+++ hardware/raspberrypi/LightHardware.js
@@ -1,3 +1,4 @@
+const { Gpio } = require('onoff');
 const log = require('../../lib/log');
 const config = require('../../lib/config');
 
@@ -12,7 +13,8 @@ class LightHardware {
 
     // should have ability to R/W  and any other custom funcs
     // such as take reading from custom hardware
-
+    this._RCPin = new Gpio(config.pins.LDRPin, 'out');
+    // charge it
     log.debug('constructing LightHardware object');
     // this._lightHardware = new LightHardware();
   }
@@ -39,19 +41,23 @@ class LightHardware {
     this._state = state;
   }
 
+  get level() {
   // # Function to measure res-cap charge time
   // def RCtime (RCPin):
   //     # Discharge capacitor
   //     GPIO.setup(RCPin, GPIO.OUT)
-  //     GPIO.output(RCPin, GPIO.LOW)
-  //     time.sleep(0.1) #give time for C to discharge
-  //     GPIO.setup(RCPin, GPIO.IN)  #set RC pin to hi impedance
-  //     # Count loops until voltage across capacitor reads high on GPIO
-  //     measurement = 0
-  //     while (GPIO.input(RCPin) == GPIO.LOW) and (measurement < 9999):
-  //         measurement += 1
-  //     return measurement
-  get level() {
+    this._RCPin = new Gpio(config.pins.LDRPin, 'out');
+    //     GPIO.output(RCPin, GPIO.LOW)
+    this._RCPin.writeSync(0);
+    //     time.sleep(0.1) #give time for C to discharge
+
+    //     GPIO.setup(RCPin, GPIO.IN)  #set RC pin to hi impedance
+    //     # Count loops until voltage across capacitor reads high on GPIO
+    //     measurement = 0
+    //     while (GPIO.input(RCPin) == GPIO.LOW) and (measurement < 9999):
+    //         measurement += 1
+    //     return measurement
+
     if (config.envName === 'development') {
       log.debug(`Get LightHardware level DEV: ${this._state}`);
     } else {
